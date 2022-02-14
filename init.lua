6b253bde162f1bc01aa349b5f4cebde5c8f4eafa
tiamat59 <tiamat59@gmail.com>
2022-02-14 10:16:51 +0100

    update conf

--- init.lua
+++ init.lua
@@ -1,32 +1,7 @@
--- Load basic conf.
-vim.cmd [[runtime base.vim]]
-
--- Map leader to space
-vim.g.mapleader = ' '
-
-local fn = vim.fn
-local execute = vim.api.nvim_command
-
--- Sensible defaults
-require('settings')
-
--- Auto install packer.nvim if not exists
-local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-end
-vim.cmd [[packadd packer.nvim]]
-vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile' -- Auto compile when there are changes in plugins.lua
-
--- Install plugins
-require('plugins')
-
--- Key mappings
-require('keymappings')
-
--- Setup language server 
-require('lsp')
-
--- Another option is to groups configuration in one folder
--- require('config')
-
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
---
+++ lua/user/colorscheme.lua
@@ -0,0 +1,8 @@
+local colorscheme = "darkplus"
+
+-- Fancy error message if plugin is missing
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
