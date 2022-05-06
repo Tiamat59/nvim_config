706e10e1f33b9e239c05857573046238d9ec9a93
tiamat59 <tiamat59@gmail.com>
2022-05-06 18:31:21 +0200

    fix treesitter

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -41,6 +41,7 @@ packer.init({
 -- Install your plugins here
 return packer.startup(function(use)
 	-- My plugins here
+
 	use("wbthomason/packer.nvim") -- Have packer manage itself
 	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
 	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
@@ -55,6 +56,14 @@ return packer.startup(function(use)
 	use("folke/which-key.nvim")
 	use("lewis6991/impatient.nvim")
 	use("lukas-reineke/indent-blankline.nvim")
+
+	--Treesitter
+    use({
+	   "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+	})
+	use("p00f/nvim-ts-rainbow")
+	use("JoosepAlviste/nvim-ts-context-commentstring")
 
 	-- Colorscheme
 	-- use 'joshdick/onedark.vim'
@@ -83,13 +92,6 @@ return packer.startup(function(use)
 	-- ueberzug is required
 	use("nvim-telescope/telescope-media-files.nvim")
 
-	--Treesitter
-    use({
-	    "nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
-	use("p00f/nvim-ts-rainbow")
-	use("JoosepAlviste/nvim-ts-context-commentstring")
 
 	-- Git
 	use("lewis6991/gitsigns.nvim")
